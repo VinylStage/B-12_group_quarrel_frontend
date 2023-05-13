baa3f37623668f2b710d93085ffcb7bd0385367b
BenCyera <dltpgml1124@gmail.com>
2023-05-14 01:12:40 +0900

    users/user_id.js수정,category.js수정

--- app/articles/category_id/page.js
+++ app/articles/category_id/page.js
@@ -164,8 +164,8 @@ export default function category() {
                         </section>
                     </section>
                 </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    
+  )
 }
