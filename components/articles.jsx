a5ce86f41d69b3dccde8ccf84d8ac5c37a1042b3
Songjimyung <jimyung0401@gmail.com>
2023-05-15 04:38:03 +0900

    followers edit

--- components/articles.jsx
+++ components/articles.jsx
@@ -3,13 +3,14 @@
 // 게시글 목록가져오는 컴포넌트
 import { useState, useEffect } from "react";
 import axios from "axios";
+// import category from "@/app/articles/[category_id]/page";
 
 function Articles({category_id}) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [category_id]);
 
   const fetchData = async () => {
     try {
@@ -20,7 +21,7 @@ function Articles({category_id}) {
           Authorization: `Bearer ${token}`,
         },
       });
-      const data = await response.json()
+      const data = response.data;
       setData(data);
       console.log(data)
       console.log(setData.data)
@@ -31,7 +32,7 @@ function Articles({category_id}) {
 
   return (
     <div>
-      {data && data.results.map((a,i) => {        
+      {data && data.map((a,i) => {        
         console.log(a)
         const title = a.title;
         const content = a.content;
